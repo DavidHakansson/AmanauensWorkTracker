8bd50ec75fedef9151d913524dc6c822d89950b2
DavidHakansson <davidhak@live.se>
2024-09-19 21:15:37 +0200

    Set up main screen

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
-# Initialize Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'auth.login'  
@@ -17,14 +16,11 @@ login_manager.login_view = 'auth.login'
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Create database tables
 with app.app_context():
     db.create_all()
 
-# Register blueprints
 app.register_blueprint(auth)
 app.register_blueprint(routes)  
 
-# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
--- routes.py
+++ routes.py
@@ -7,7 +7,6 @@ routes = Blueprint('routes', __name__)
 @routes.route('/')
 @login_required
 def home():
-    # Get the user's logged work hours
     work_hours = WorkHour.query.filter_by(user_id=current_user.id).all()
     return render_template('index.html', work_hours=work_hours)
 
@@ -16,8 +15,8 @@ def home():
 def submit_hours():
     date = request.form['date']
     hours = request.form['hours']
-    description = request.form['description']  # Include description
+    description = request.form['description'] 
     work_hour = WorkHour(user_id=current_user.id, date=date, hours=hours, description=description)
     db.session.add(work_hour)
     db.session.commit()
-    return redirect(url_for('routes.home'))  # Redirect to home after submission
+    return redirect(url_for('routes.home'))  
